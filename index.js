3f13fb80503adf7aeb9cf5617dc9ffd94386575c
Dean Howe <hideanhowe@gmail.com>
2025-12-18 20:49:28 +0000

    solution

--- index.js
+++ index.js
@@ -7,3 +7,22 @@
 
 // 3	1
 // 4	2
+
+function rotate(matrix) {
+  console.log(matrix);
+  // create a new empty matrix
+  const newMatrix = [];
+  // loop over column indicies
+  for (let i = 0; i < matrix.length; i++) {
+    const row = [];
+    for (let l = matrix.length - 1; l >= 0; l--) {
+      row.push(matrix[l][i]);
+    }
+    newMatrix.push(row);
+  }
+  // Inside that loop, walk through rows from last to first
+  // push values into a new row
+  // push that row into the result
+  console.log(newMatrix);
+  return newMatrix;
+}
